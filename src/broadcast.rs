aca9643b13c016a469a9375fca5a902cfbd55886
Bhargav <bIgBV@users.noreply.github.com>
2019-04-08 13:30:53 -0500

    Flesh out `TransmitQueue` implementation (#7)
    * Add get broadcasts method
    
    * Doesn't work right now
    
    * More debugging
    
    * Fix broadcast iteration
    
    * Add simple tests for basic conditions
    
    * Add documentation for the get algorithm
    
    * Remove quickcheck and simplify some logic
    
    * Fix cargo.toml and add additional docs
    
    * Review comments

--- src/broadcast.rs
+++ src/broadcast.rs
@@ -1,14 +1,17 @@
 use std::cmp::Ordering;
 use std::collections::{hash_map::DefaultHasher, BTreeSet};
 
+use bincode::{serialized_size, Result};
 use cuckoofilter::CuckooFilter;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
 // An empty struct for now.
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Message {
     Joined,
+    Alive,
+    Leave,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
@@ -26,7 +29,7 @@ impl Broadcast {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Debug, Clone, Eq)]
 pub struct LimitedBroadcast {
     transmits: u64,
     id: u64,
@@ -44,9 +47,26 @@ impl Ord for LimitedBroadcast {
             return Ordering::Less;
         } else if self.transmits > other.transmits {
             return Ordering::Greater;
+        } else if self.id < other.id {
+            return Ordering::Greater;
+        } else if self.id > other.id {
+            return Ordering::Less;
+        } else {
+            // Never going to happen as id is monotonically increasing.
+            Ordering::Equal
         }
+    }
+}
 
-        self.id.cmp(&other.id)
+impl PartialEq for LimitedBroadcast {
+    fn eq(&self, other: &LimitedBroadcast) -> bool {
+        self.transmits == other.transmits && self.id == other.id
+    }
+}
+
+impl PartialEq<Broadcast> for LimitedBroadcast {
+    fn eq(&self, other: &Broadcast) -> bool {
+        self.broadcast.uuid == other.uuid
     }
 }
 
@@ -68,6 +88,14 @@ impl LimitedBroadcast {
     fn transmits(&self) -> u64 {
         self.transmits
     }
+
+    fn gen(transmits: u64, id: u64, broadcast: Broadcast) -> LimitedBroadcast {
+        Self {
+            transmits,
+            id,
+            broadcast,
+        }
+    }
 }
 
 pub struct TransmitQueue {
@@ -76,6 +104,9 @@ pub struct TransmitQueue {
     gen: u64,
 }
 
+/// TransmitQueue is used to queue messages to broadcast to the cluster but limits the number of
+/// transmits per message. It prioritizes messages with lower transmit counts and newer version of
+/// the same message. This means that the items yielded by the queue will be ordered newest first.
 impl TransmitQueue {
     pub fn new() -> Self {
         Self {
@@ -85,6 +116,29 @@ impl TransmitQueue {
         }
     }
 
+    /// Return the number of elements currently present in the queue.
+    pub fn len(&self) -> usize {
+        self.set.len()
+    }
+
+    /// Add the given `LimitedBroadcast` to the queue.
+    fn add(&mut self, val: LimitedBroadcast) {
+        self.filter.add(&val.broadcast.uuid);
+        self.set.insert(val);
+    }
+
+    /// Remove the given `LimitedBroadcast` from the queue.
+    fn delete(&mut self, val: &LimitedBroadcast) {
+        self.filter.delete(&val.broadcast.uuid);
+        self.set.remove(&val);
+
+        if self.set.is_empty() {
+            self.gen = 0;
+        }
+    }
+
+    /// Enqueues a broadcast to be disseminated. If a Broadcast with the same UUID already exists,
+    /// it will be removed and the new version will be inserted.
     pub fn enqueue(&mut self, broadcast: Broadcast) {
         let uuid = broadcast.uuid;
 
@@ -109,36 +163,154 @@ impl TransmitQueue {
             }
         }
 
-        self.filter.add(&limited_broadcast.broadcast.uuid);
-        self.set.insert(limited_broadcast);
+        self.add(limited_broadcast);
     }
 
-    pub fn get_broadcasts(&mut self) -> Vec<&LimitedBroadcast> {
-        self.set.iter().collect()
+    /// Returns a list of `LimitedBroadcast`s ordered by newest first. The limit parameter limits
+    /// the total number of `LimitedBroadcast`s returned. The size being counted is the
+    /// `serialized_size` of the `Broadcast` type.
+    pub fn get_broadcasts(&mut self, limit: u64) -> Result<Vec<LimitedBroadcast>> {
+        let int_max = std::u64::MAX;
+        let mut used: i64 = 0;
+
+        let mut reinsert = vec![];
+        let mut broadcasts = vec![];
+
+        let joined = Broadcast::new(Message::Joined);
+
+        let min_item = LimitedBroadcast::gen(0, int_max, joined.clone());
+        let max_item = LimitedBroadcast::gen(int_max, int_max, joined.clone());
+
+        let min = self.set.iter().next().unwrap_or(&min_item).transmits;
+        let max = self.set.iter().next_back().unwrap_or(&max_item).transmits;
+
+        // Try to get all transmits within a given tier. We do this by getting the min and max
+        // number of transmits in the queue, and iterating over each transmit tier. Each iteration
+        // of this loop, we see if there are any broadcasts which fit the remaining size in the
+        // given transmit tier and add it to the `broadcasts` vec. These items are added to the
+        // prune list. If the item hasn't been transmitted `limit` (currently 5) times, it is
+        // reinserted into the queue.
+        for transmits in min..max + 1 {
+            let mut free: i64 = limit as i64 - used;
+
+            if free <= 0 {
+                break;
+            }
+
+            let start = LimitedBroadcast::gen(transmits, int_max, joined.clone());
+            // Ranges in Rust are Include(min)..Exclude(max), so we need to add by one to get tier
+            // i
+            let end = LimitedBroadcast::gen(transmits + 1, int_max, joined.clone());
+
+            let mut prune = vec![];
+
+            for item in self.set.range(start..end) {
+                let size = serialized_size(&item.broadcast)? as i64;
+                if size as i64 > free {
+                    // Ignore this broadcast as it won't fit
+                    continue;
+                }
+
+                // Update sizes
+                used += size;
+                free -= size;
+
+                broadcasts.push(item.clone());
+                prune.push(item.clone());
+
+                // TODO: make this parameter configurable
+                if item.transmits + 1 <= 5 {
+                    // We reinsert after the broadcasts vec if filled as if we reinsert into the
+                    // queue here, the next iteration of the loop might consider the same item
+                    // again.
+                    reinsert.push(item.clone())
+                }
+            }
+
+            for item in prune.iter_mut() {
+                self.delete(item);
+            }
+        }
+
+        for mut item in reinsert {
+            item.transmits += 1;
+            self.add(item);
+        }
+
+        Ok(broadcasts)
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::{
+        self,
+        distributions::{Distribution, Standard},
+        Rng,
+    };
+
+    impl Distribution<Message> for Standard {
+        fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Message {
+            match rng.gen_range(0, 3) {
+                0 => Message::Joined,
+                1 => Message::Alive,
+                _ => Message::Leave,
+            }
+        }
+    }
+
+    fn gen_broadcasts(n: usize) -> Vec<Broadcast> {
+        (0..n).map(|_| Broadcast::new(rand::random())).collect()
+    }
 
     #[test]
-    fn invalidate_exisitng() {
-        let broadcast = Broadcast::new(Message::Joined);
-        let clone = broadcast.clone();
+    fn invalidate_existing() {
+        let broadcasts = gen_broadcasts(1);
+        let mut queue = TransmitQueue::new();
+
+        queue.enqueue(broadcasts[0].clone());
+        let packet = queue.get_broadcasts(1500).unwrap();
+        assert_eq!(packet.len(), 1);
+
+        queue.enqueue(broadcasts[0].clone());
+        let packet = queue.get_broadcasts(1500).unwrap();
+        assert_eq!(packet.len(), 1);
+    }
+
+    #[test]
+    fn get_to_limit() {
+        let broadcasts = gen_broadcasts(5);
+        let num_broadcasts = 3;
+        let size = serialized_size(&Broadcast::new(Message::Joined)).unwrap();
 
         let mut queue = TransmitQueue::new();
 
-        queue.enqueue(broadcast);
+        broadcasts.into_iter().for_each(|b| queue.enqueue(b));
 
-        let broadcasts = queue.get_broadcasts();
+        let packets = queue.get_broadcasts(size * num_broadcasts).unwrap();
+        assert_eq!(packets.len(), num_broadcasts as usize);
+    }
 
-        assert_eq!(broadcasts.len(), 1);
+    #[test]
+    fn empty_queue() {
+        let broadcasts = gen_broadcasts(5);
+        let num_broadcasts = 3;
+        let size = serialized_size(&Broadcast::new(Message::Joined)).unwrap();
 
-        queue.enqueue(clone);
+        let mut queue = TransmitQueue::new();
 
-        let broadcasts = queue.get_broadcasts();
+        broadcasts.into_iter().for_each(|b| queue.enqueue(b));
+        let mut counter = 0;
 
-        assert_eq!(broadcasts.len(), 1);
+        while queue.len() > 0 {
+            let _packets = queue.get_broadcasts(size * num_broadcasts).unwrap();
+            counter += 1;
+        }
+
+        // This is based on the hard coded limit of 5 transmits per broadcast. We have 5 items in
+        // the queue, `get_broadcast` call gets 3 items of 1 tier, followed by 2 items of 1 tier
+        // and 1 item of the next tier, and takes 10 calls to empty the queue.
+        assert_eq!(counter, 10);
     }
 }

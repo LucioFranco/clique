c2a2e415b0f060c952674cf7860d9fcfce4c773d
Bhargav Voleti <bhargav.voleti93@gmail.com>
2019-05-17 16:36:54 -0500

    More UniquePool design

--- clique-grpc/src/lib.rs
+++ clique-grpc/src/lib.rs
@@ -1,14 +1,22 @@
-use futures::Future;
-use http::Uri;
-use http_body::Body as HttpBody;
+use futures::{
+    stream::futures_unordered::FuturesUnordered,
+    sync::{oneshot::channel, Sender},
+    Async, Future, Poll,
+};
+use http::{Request, Response, Uri};
 use hyper::client::connect::{Destination, HttpConnector};
-use tokio_buf::BufStream;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 use tower_hyper::{
-    client::{self, Connection},
+    body::Body,
+    client::{self, future::ResponseFuture, Connection},
     util,
 };
 use tower_request_modifier;
+use tower_service::Service;
 use tower_util::MakeService;
+
+use std::{collections::HashMap, hash::Hash};
 
 pub mod clique_proto {
     include!(concat!(env!("OUT_DIR"), "/messaging.rs"));
@@ -16,33 +24,81 @@ pub mod clique_proto {
 
 pub use self::clique_proto::*;
 
-use clique::messaging::{Broadcast, RapidResponseFuture};
+#[derive(Clone)]
+struct Conn(Connection<Body>);
 
-struct CliqueClient {
-    conn: Connection<tower_hyper::body::Body>,
+struct PoolReq {
+    uri: Uri,
+    req: Request<Body>,
 }
 
-impl CliqueClient {
+pub struct UniquePool {
+    pool: HashMap<PoolReq, Conn>,
+    channels: HashMap<PoolReq, Sender<Response>>,
+    buffering: usize,
+    max_buffering: usize,
+    // runner: FuturesUnordered<ResponseFuture>, // Should this be a `ConnectFuture`?
+}
+
+impl UniquePool {
+    pub fn new(max: usize) -> UniquePool {
+        UniquePool {
+            pool: HashMap::new(),
+            buffering: 0,
+            max_buffering: max,
+            // runner: FuturesUnordered::new(),
+        }
+    }
+}
+
+impl Service<Request<Body>> for UniquePool {
+    type Response = Response<Body>;
+    type Error = ();
+    type Future = Box<dyn Future<Item = Response<Body>, Error = ()>>;
+
+    fn poll_ready(&mut self) -> Poll<(), Self::Error> {
+        if self.buffering >= self.max_buffering {
+            Err(())
+        } else {
+            Ok(Async::Ready(()))
+        }
+    }
+
+    fn call(&mut self, req: PoolReq) -> Self::Future {
+        if let Some(conn) = self.pool.get(req.uri) {
+            // We have already established a connection to this destination
+            // We pull the connection out and check if it is ready via `poll_ready`
+            //     * if it isn't we wrap it in a future which just checks if the connection is ready
+            //       and increase the buffer count
+            //          * if it is, notify the pool?
+            //          * Or make the reqeust itself?
+            //     * If it is, we make the request?
+        } else {
+            // The connection doesn't exist, so we need to establish it, and then wait for it to resolve
+            // and once it does, we need to make the request.
+            // All this has to be done in a non-blocking way.
+        }
+    }
+}
+
+struct Broadcaster {
+    recipients: Vec<Endpoint>,
+}
+
+impl Broadcaster {
     fn init(uri: Uri) -> CliqueClient {
-        let dst = Destination::try_from_uri(uri).unwrap();
-        let connector = util::Connector::new(HttpConnector::new(4));
-        let settings = client::Builder::new().http2_only(true).clone();
-        let mut make_client = client::Connect::with_builder(connector, settings);
+        CliqueClient {
+            reciepients: Vec::new(),
+        }
+    }
 
-        let clique_client = make_client
-            .make_service(dst)
-            .map_err(|e| panic!("Connect error: {:?}", e))
-            .and_then(|conn| {
-                use clique_proto::client::MembershipService;
+    fn set_membership(&mut self, endpoints: Vec<Endpoint>) {
+        let mut rng = thread_rng();
+        self.recipients = endpoints.drain().collect();
+        &self.recipients[..].shuffle(&mut rng);
+    }
 
-                let conn = tower_request_modifier::Builder::new()
-                    .set_origin(uri)
-                    .build(conn)
-                    .unwrap();
-
-                MembershipService::new(conn).ready()
-            });
-
-        CliqueClient { conn: make_client }
+    fn broadcast(&mut self, request: RapidRequest) {
+        self.call(request);
     }
 }

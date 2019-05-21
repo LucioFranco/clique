7c4f19916a9ec9e42d7c81c70c3d5053536684bb
Bhargav Voleti <bhargav.voleti93@gmail.com>
2019-05-21 18:03:17 -0500

    Make generics soup

--- clique-grpc/src/lib.rs
+++ clique-grpc/src/lib.rs
@@ -1,60 +1,121 @@
 use futures::{
     stream::futures_unordered::FuturesUnordered,
-    sync::{oneshot::channel, Sender},
+    sync::{mpsc, oneshot},
     Async, Future, Poll,
 };
-use http::{Request, Response, Uri};
+use http::Uri;
 use hyper::client::connect::{Destination, HttpConnector};
-use rand::seq::SliceRandom;
-use rand::thread_rng;
+use tokio;
 use tower_hyper::{
     body::Body,
-    client::{self, future::ResponseFuture, Connection},
+    client::{self, Connection},
     util,
 };
 use tower_request_modifier;
 use tower_service::Service;
 use tower_util::MakeService;
 
-use std::{collections::HashMap, hash::Hash};
+use std::collections::HashMap;
 
 pub mod clique_proto {
     include!(concat!(env!("OUT_DIR"), "/messaging.rs"));
 }
 
-pub use self::clique_proto::*;
-
-#[derive(Clone)]
-struct Conn(Connection<Body>);
+use clique_proto::client as ProtoClient;
+use clique_proto::RapidRequest;
 
 struct PoolReq {
     uri: Uri,
-    req: Request<Body>,
+    req: RapidRequest,
 }
 
-pub struct UniquePool {
-    pool: HashMap<PoolReq, Conn>,
-    channels: HashMap<PoolReq, Sender<Response>>,
-    buffering: usize,
-    max_buffering: usize,
-    // runner: FuturesUnordered<ResponseFuture>, // Should this be a `ConnectFuture`?
+struct FuturesRunner<T: Future> {
+    inner: FuturesUnordered<T>,
+    rx: mpsc::UnboundedReceiver<(T, oneshot::Sender<T::Item>)>,
 }
 
-impl UniquePool {
-    pub fn new(max: usize) -> UniquePool {
-        UniquePool {
-            pool: HashMap::new(),
-            buffering: 0,
-            max_buffering: max,
-            // runner: FuturesUnordered::new(),
+impl<T> FuturesRunner<T>
+where
+    T: Future,
+{
+    pub fn new() -> Enqueuer<T, T> {
+        let (tx, rx) = mpsc::unbounded();
+        tokio::spawn(FuturesRunner {
+            inner: FuturesUnordered::new(),
+            rx,
+        });
+
+        Enqueuer { chan: tx }
+    }
+}
+
+impl<F> Future for FuturesRunner<F>
+where
+    F: Future<Item = F::Item, Error = F::Error>,
+{
+    type Item = F::Item;
+    type Error = F::Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        match self.inner.poll() {
+            Ok(Async::Ready(None)) => Ok(Async::NotReady), // There are no futures to run
+            Ok(Async::Ready(Some(val))) => Ok(Async::Ready(val)),
+            Ok(Async::NotReady) => Ok(Async::NotReady),
+            Err(e) => Err(e),
         }
     }
 }
 
-impl Service<Request<Body>> for UniquePool {
-    type Response = Response<Body>;
-    type Error = ();
-    type Future = Box<dyn Future<Item = Response<Body>, Error = ()>>;
+#[derive(Debug, Clone)]
+struct Enqueuer<T, F> {
+    chan: mpsc::UnboundedSender<(F, oneshot::Sender<T>)>,
+}
+
+impl<T, F, Request> Service<Request> for Enqueuer<T, F>
+where
+    T: Service<Request> + Clone,
+    F: Future,
+{
+    type Response = T::Response;
+    type Error = T::Error;
+    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
+
+    fn poll_ready(&mut self) -> Poll<(), Self::Error> {
+        self.chan.poll_ready()
+    }
+
+    fn call(&mut self, req: Request) -> Self::Future {
+        let (tx, rx) = oneshot::channel();
+        self.chan.unbouned_send((req, tx))
+    }
+}
+
+pub struct UniquePool<T, F> {
+    pool: HashMap<Uri, T>,
+    buffering: usize,
+    max_buffering: usize,
+    enqueuer: Enqueuer<T, F>,
+}
+
+impl<T, F> UniquePool<T, F> {
+    pub fn new(max: usize) -> UniquePool<T, F> {
+        UniquePool {
+            pool: HashMap::new(),
+            buffering: 0,
+            max_buffering: max,
+            enqueuer: FuturesRunner::new(),
+        }
+    }
+}
+
+impl<T, F, Request> Service<Request> for UniquePool<T, F>
+where
+    T: Service<Request> + Clone,
+    F: Future,
+{
+    type Response = T::Response;
+    type Error = T::Error;
+    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
 
     fn poll_ready(&mut self) -> Poll<(), Self::Error> {
         if self.buffering >= self.max_buffering {
@@ -64,41 +125,36 @@ impl Service<Request<Body>> for UniquePool {
         }
     }
 
-    fn call(&mut self, req: PoolReq) -> Self::Future {
+    fn call(&mut self, req: Request) -> Self::Future {
         if let Some(conn) = self.pool.get(req.uri) {
             // We have already established a connection to this destination
-            // We pull the connection out and check if it is ready via `poll_ready`
-            //     * if it isn't we wrap it in a future which just checks if the connection is ready
-            //       and increase the buffer count
-            //          * if it is, notify the pool?
-            //          * Or make the reqeust itself?
-            //     * If it is, we make the request?
+            ProtoClient::MembershipService::new(conn)
+                .and_then(|client| client.send_request(req.req))
         } else {
             // The connection doesn't exist, so we need to establish it, and then wait for it to resolve
             // and once it does, we need to make the request.
             // All this has to be done in a non-blocking way.
+            self.buffering += 1;
+
+            let dst = Destination::try_from_uri(req.uri.clone()).unwrap();
+            let connector = util::Connector::new(HttpConnector::new(4));
+            let settings = client::Builder::new().http2_only(true).clone();
+            let mut make_client = client::Connect::with_builder(connector, settings);
+
+            make_client
+                .make_service(dst) // we need to enqueue
+                .map_err(|e| panic!("Unable to connect to destination"))
+                .and_then(|conn| {
+                    use ProtoClient::MembershipService;
+
+                    let conn = tower_request_modifier::Builder::new()
+                        .set_origin(req.uri)
+                        .build(conn)
+                        .unwrap();
+
+                    MembershipService::new(conn).ready() // This should be run on the background process
+                })
+                .and_then(|mut client| client.send_request(req.request))
         }
-    }
-}
-
-struct Broadcaster {
-    recipients: Vec<Endpoint>,
-}
-
-impl Broadcaster {
-    fn init(uri: Uri) -> CliqueClient {
-        CliqueClient {
-            reciepients: Vec::new(),
-        }
-    }
-
-    fn set_membership(&mut self, endpoints: Vec<Endpoint>) {
-        let mut rng = thread_rng();
-        self.recipients = endpoints.drain().collect();
-        &self.recipients[..].shuffle(&mut rng);
-    }
-
-    fn broadcast(&mut self, request: RapidRequest) {
-        self.call(request);
     }
 }

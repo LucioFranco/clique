b89e463330f5cbeff3696586c38eaf66f5b67117
Bhargav Voleti <bhargav.voleti93@gmail.com>
2019-05-22 00:37:56 -0500

    Remove enqueuer, and clean up the file

--- clique-grpc/src/lib.rs
+++ clique-grpc/src/lib.rs
@@ -29,51 +29,51 @@ struct PoolReq {
     req: RapidRequest,
 }
 
-impl BackgroundJob for PoolReq {
-    type Req = Uri;
-    type Resp = Connection<Body>;
+struct FuturesRunner<Request, T: Service<Request>> {
+    inner: FuturesUnordered<Box<dyn Future<Item = T::Response, Error = ()>>>,
+    rx: mpsc::UnboundedReceiver<(Request, oneshot::Sender<T::Response>)>,
 }
 
-struct FuturesRunner<T: BackgroundJob> {
-    inner: FuturesUnordered<Box<dyn Future<Item = T::Resp, Error = ()>>>,
-    rx: mpsc::UnboundedReceiver<(T::Req, oneshot::Sender<T::Resp>)>,
-}
-
-impl<T> FuturesRunner<T>
+impl<Request, T> FuturesRunner<Request, T>
 where
-    T: BackgroundJob,
+    T: Service<Request> + Clone,
+    Request: Address,
 {
-    pub fn new() -> Enqueuer<T> {
+    pub fn new() -> mpsc::UnboundedSender<(Request, oneshot::Sender<T::Response>)> {
         let (tx, rx) = mpsc::unbounded();
         tokio::spawn(FuturesRunner {
             inner: FuturesUnordered::new(),
             rx,
         });
 
-        Enqueuer { chan: tx }
+        tx
     }
 
     fn poll_enqueue(&mut self) {
-        self.rx.for_each(|uri| {
-            let dst = Destination::try_from_uri(uri.clone()).unwrap();
-            let connector = util::Connector::new(HttpConnector::new(4));
-            let settings = client::Builder::new().http2_only(true).clone();
-            let mut make_client = client::Connect::with_builder(connector, settings);
+        self.rx
+            .for_each(|uri| {
+                let dst = Destination::try_from_uri(uri.clone()).unwrap();
+                let connector = util::Connector::new(HttpConnector::new(4));
+                let settings = client::Builder::new().http2_only(true).clone();
+                let mut make_client = client::Connect::with_builder(connector, settings);
 
-            let client = make_client
-                .make_service(dst) // we need to enqueue
-                .map_err(|e| panic!("Unable to connect to destination"));
+                let client = make_client
+                    .make_service(dst) // we need to enqueue
+                    .map_err(|e| panic!("Unable to connect to destination"));
 
-            self.inner.push(Box::new(client));
-        });
+                self.inner.push(Box::new(client));
+                Ok(())
+            })
+            .poll();
     }
 }
 
-impl<T> Future for FuturesRunner<T>
+impl<Request, T> Future for FuturesRunner<Request, T>
 where
-    T: BackgroundJob,
+    T: Service<Request> + Clone,
+    Request: Address,
 {
-    type Item = T::Resp;
+    type Item = T::Response;
     type Error = ();
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
@@ -89,51 +89,22 @@ where
     }
 }
 
-trait BackgroundJob {
-    type Req: Clone;
-    type Resp;
-}
-
-#[derive(Debug, Clone)]
-struct Enqueuer<T: BackgroundJob> {
-    chan: mpsc::UnboundedSender<(T::Req, oneshot::Sender<T::Resp>)>,
-}
-
-impl<T> Service<T::Req> for Enqueuer<T>
+pub struct UniquePool<Request, T>
 where
-    T: BackgroundJob,
+    T: Service<Request> + Clone,
 {
-    type Response = T::Resp;
-    type Error = ();
-    type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
-
-    fn poll_ready(&mut self) -> Poll<(), Self::Error> {
-        self.chan.poll_ready()
-    }
-
-    fn call(&mut self, req: T::Req) -> Self::Future {
-        let (tx, rx) = oneshot::channel();
-        self.chan.unbouned_send((req, tx))
-    }
-}
-
-pub struct UniquePool<Req, T>
-where
-    Req: BackgroundJob,
-    T: Service<Req>,
-{
-    pool: HashMap<Uri, T>,
+    pool: HashMap<Uri, T::Response>,
     buffering: usize,
     max_buffering: usize,
-    enqueuer: Enqueuer<Req>,
+    enqueuer: mpsc::UnboundedSender<(Request, oneshot::Sender<T::Response>)>,
 }
 
-impl<Req, T> UniquePool<Req, T>
+impl<Request, T> UniquePool<Request, T>
 where
-    Req: BackgroundJob,
-    T: Service<Req>,
+    T: Service<Request> + Clone,
+    Request: Address,
 {
-    pub fn new(max: usize) -> UniquePool<Req, T> {
+    pub fn new(max: usize) -> UniquePool<Request, T> {
         UniquePool {
             pool: HashMap::new(),
             buffering: 0,
@@ -143,13 +114,21 @@ where
     }
 }
 
-impl<Req, T> Service<Req> for UniquePool<Req, T>
+trait Address {
+    type Address: Clone;
+    type Payload: Clone;
+
+    fn get_uri(&self) -> Self::Address;
+    fn get_payload(&self) -> Self::Payload;
+}
+
+impl<Request, T> Service<Request> for UniquePool<Request, T>
 where
-    T: Service<Req> + Clone,
-    Req: BackgroundJob,
+    T: Service<Request> + Clone,
+    Request: Address,
 {
     type Response = T::Response;
-    type Error = T::Error;
+    type Error = (); // Need better error handling
     type Future = Box<Future<Item = Self::Response, Error = Self::Error>>;
 
     fn poll_ready(&mut self) -> Poll<(), Self::Error> {
@@ -160,8 +139,8 @@ where
         }
     }
 
-    fn call(&mut self, req: Req) -> Self::Future {
-        if let Some(conn) = self.pool.get(req.uri) {
+    fn call(&mut self, req: Request) -> Self::Future {
+        if let Some(conn) = self.pool.get(req.get_uri()) {
             // We have already established a connection to this destination
             ProtoClient::MembershipService::new(conn)
                 .and_then(|client| client.send_request(req.req))
@@ -171,22 +150,22 @@ where
             // All this has to be done in a non-blocking way.
             self.buffering += 1;
 
-            let request = req as PoolReq;
-
             self.enqueuer
-                .call(request.uri)
+                .unbounded_send(request.get_uri())
                 .map_err(|e| panic!("Unable to connect to destination"))
                 .and_then(|conn| {
                     use ProtoClient::MembershipService;
 
                     let conn = tower_request_modifier::Builder::new()
-                        .set_origin(req.uri)
+                        .set_origin(req.get_uri())
                         .build(conn)
                         .unwrap();
 
-                    MembershipService::new(conn).ready() // This should be run on the background process
+                    Ok(MembershipService::new(conn)) // This should be run on the background process
                 })
-                .and_then(|mut client| client.send_request(req.request))
+                .and_then(|mut client| client.send_request(req.get_payload()))
+                .map_err(|e| println!("ERROR making request"))
+                .map(|val| val.ok())
         }
     }
 }

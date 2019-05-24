fbc7d838e3c860b89c7cf6f332e8e46530c88f29
Bhargav Voleti <bhargav.voleti93@gmail.com>
2019-05-24 13:54:05 -0500

    Add `Into<Uri>` bounds for trait

--- clique-grpc/src/lib.rs
+++ clique-grpc/src/lib.rs
@@ -26,7 +26,7 @@ use clique_proto::RapidRequest;
 
 struct Message {
     uri: Uri,
-    payload: RapidRequest,
+    req: RapidRequest,
 }
 
 pub struct UniquePool<Request, T>
@@ -36,13 +36,15 @@ where
     pool: HashMap<Uri, T::Response>,
     buffering: usize,
     max_buffering: usize,
+    enqueuer: mpsc::UnboundedSender<(Request, oneshot::Sender<T::Response>)>,
 }
 
-impl<Message, T> UniquePool<Message, T>
+impl<Request, T> UniquePool<Request, T>
 where
-    T: Service<Message> + Clone,
+    T: Service<Request> + Clone,
+    Request: PoolRequest,
 {
-    pub fn new(max: usize) -> UniquePool<Message, T> {
+    pub fn new(max: usize) -> UniquePool<Request, T> {
         UniquePool {
             pool: HashMap::new(),
             buffering: 0,
@@ -51,9 +53,31 @@ where
     }
 }
 
-impl<Message, T> Service<Message> for UniquePool<Message, T>
+trait PoolRequest {
+    type Address: Into<Uri> + Clone;
+    type Payload: Clone;
+
+    fn get_uri(&self) -> Self::Address;
+    fn get_payload(&self) -> Self::Payload;
+}
+
+impl PoolRequest for Message {
+    type Address = Uri;
+    type Payload = RapidRequest;
+
+    fn get_uri(&self) -> Self::Address {
+        self.uri
+    }
+
+    fn get_payload(&self) -> Self::Payload {
+        self.req
+    }
+}
+
+impl<Request, T> Service<Request> for UniquePool<Request, T>
 where
-    T: Service<Message> + Clone,
+    T: Service<Request> + Clone,
+    Request: PoolRequest,
 {
     type Response = T::Response;
     type Error = Box<std::error::Error + Send + Sync + 'static>; // Need better error handling
@@ -61,24 +85,24 @@ where
 
     fn poll_ready(&mut self) -> Poll<(), Self::Error> {
         if self.buffering >= self.max_buffering {
-            Err(().into())
+            Err(Box::new(()))
         } else {
             Ok(Async::Ready(()))
         }
     }
 
-    fn call(&mut self, request: Message) -> Self::Future {
-        if let Some(conn) = self.pool.get(&request.uri) {
+    fn call(&mut self, req: Request) -> Self::Future {
+        if let Some(conn) = self.pool.get(&req.get_uri().into()) {
             // We have already established a connection to this destination
             ProtoClient::MembershipService::new(conn)
-                .and_then(|client| Box::new(client.send_request(request.payload)))
+                .and_then(|client| client.send_request(req.get_payload()))
         } else {
             // The connection doesn't exist, so we need to establish it, and then wait for it to resolve
             // and once it does, we need to make the request.
             // All this has to be done in a non-blocking way.
             self.buffering += 1;
 
-            let dst = Destination::try_from_uri(request.uri.clone()).unwrap();
+            let dst = Destination::try_from_uri(req.get_uri().into().clone()).unwrap();
             let connector = util::Connector::new(HttpConnector::new(4));
             let settings = client::Builder::new().http2_only(true).clone();
             let mut make_client = client::Connect::with_builder(connector, settings);
@@ -88,15 +112,15 @@ where
                 .map_err(Into::into)
                 .and_then(move |conn| {
                     let conn = tower_request_modifier::Builder::new()
-                        .set_origin(request.uri)
+                        .set_origin(req.get_uri().into())
                         .build()
                         .unwrap();
 
                     self.pool.insert(conn);
                     Ok(ProtoClient::MembershipService::new(conn))
                 })
-                .and_then(|mut client| Box::new(client.send_request(request.payload)))
-                .map_err(Into::into)
+                .and_then(|mut client| client.send_request(req.get_payload()))
+                .map_err(Into::into);
         }
     }
 }

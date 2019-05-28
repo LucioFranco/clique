f4a8b7d56ac53a6e94d676d38a7defb2ce9f0f69
Bhargav Voleti <bhargav.voleti93@gmail.com>
2019-05-27 23:57:40 -0500

    Clean up file and experiment with error handling

--- clique-grpc/src/lib.rs
+++ clique-grpc/src/lib.rs
@@ -18,44 +18,6 @@ pub mod clique_proto {
     include!(concat!(env!("OUT_DIR"), "/messaging.rs"));
 }
 
-use clique_proto::client as ProtoClient;
-use clique_proto::{RapidRequest, RapidResponse};
-
-struct Message {
-    uri: Uri,
-    payload: RapidRequest,
-}
-
-trait PoolRequest<T> {
-    type Address: Into<Uri> + Clone + Eq + Hash;
-    type Payload: Into<tower_grpc::Request<T>> + Clone;
-
-    fn get_uri(&self) -> Self::Address;
-    fn get_payload(&self) -> Self::Payload;
-}
-
-impl<T> PoolRequest<T> for Message
-where
-    tower_grpc::Request<T>: From<RapidRequest>,
-{
-    type Address = Uri;
-    type Payload = RapidRequest;
-
-    fn get_uri(&self) -> Self::Address {
-        self.uri
-    }
-
-    fn get_payload(&self) -> Self::Payload {
-        self.payload
-    }
-}
-
-impl From<RapidRequest> for tower_grpc::Request<RapidRequest> {
-    fn from(req: RapidRequest) -> Request<RapidRequest> {
-        Request::new(req)
-    }
-}
-
 pub struct UniquePool<S, T, R>
 where
     S: MakeService<T, R> + Clone,
@@ -82,15 +44,15 @@ trait Key<T> {
     fn get_key(&self) -> T;
 }
 
-impl<S, T, R> Service<R> for UniquePool<S, T, R>
+impl<M, T, R> Service<R> for UniquePool<M, T, R>
 where
-    S: MakeService<T, R> + Clone,
+    M: MakeService<T, R> + Clone,
     T: Hash + Eq,
     R: Key<T>,
-    S::Service: Clone,
+    M::Service: Clone,
 {
-    type Response = S::Response;
-    type Error = S::Error; // Need better error handling
+    type Response = M::Response;
+    type Error = (); // Need better error handling
     type Future = Box<dyn Future<Item = Self::Response, Error = Self::Error>>;
 
     fn poll_ready(&mut self) -> Poll<(), Self::Error> {
@@ -99,17 +61,18 @@ where
 
     fn call(&mut self, req: R) -> Self::Future {
         if let Some(svc) = self.pool.get(&req.get_key()) {
-            Box::new(svc.call(req))
+            Box::new(svc.call(req).map_err(|_| ()))
         } else {
             Box::new(
                 self.maker
                     .clone()
                     .make_service(req.get_key())
-                    .map_err(Into::into)
-                    .map(|svc| {
+                    .map_err(|_| ())
+                    .and_then(|svc| {
                         self.pool.insert(req.get_key(), svc.clone());
                         svc.call(req)
-                    }),
+                    })
+                    .map_err(|_| ()),
             )
         }
     }

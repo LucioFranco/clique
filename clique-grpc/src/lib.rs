080dfbbed82884f5732edbd9f14dd2bf077928b2
Bhargav Voleti <bhargav.voleti93@gmail.com>
2019-05-28 11:14:06 -0500

    Try to fix error handling

--- clique-grpc/src/lib.rs
+++ clique-grpc/src/lib.rs
@@ -44,15 +44,19 @@ pub trait Key<T> {
     fn get_key(&self) -> T;
 }
 
+type Error = Box<std::error::Error + Send + Sync + 'static>;
+
 impl<M, T, R> Service<R> for UniquePool<M, T, R>
 where
     M: MakeService<T, R> + Clone,
     T: Hash + Eq,
     R: Key<T>,
     M::Service: Clone,
+    M::MakeError: Into<Error>,
+    M::Error: Into<Error>,
 {
     type Response = M::Response;
-    type Error = (); // Need better error handling
+    type Error = Error; // Need better error handling
     type Future = Box<dyn Future<Item = Self::Response, Error = Self::Error>>;
 
     fn poll_ready(&mut self) -> Poll<(), Self::Error> {
@@ -61,18 +65,18 @@ where
 
     fn call(&mut self, req: R) -> Self::Future {
         if let Some(svc) = self.pool.get(&req.get_key()) {
-            Box::new(svc.call(req).map_err(|_| ()))
+            Box::new(svc.call(req).map_err(Into::into))
         } else {
             let fut = self
                 .maker
                 .clone()
                 .make_service(req.get_key())
-                .map_err(|_| ())
+                .map_err(Into::into)
                 .and_then(|svc| {
                     self.pool.insert(req.get_key(), svc.clone());
                     svc.call(req)
                 })
-                .map_err(|_| ());
+                .map_err(Into::into);
             Box::new(fut)
         }
     }

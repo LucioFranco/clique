0d8478d15fa41529fec32b2037662a33a2b238c2
Lucio Franco <luciofranco14@gmail.com>
2019-07-12 00:59:57 -0400

    Add the rest of membership view tests

--- src/common.rs
+++ src/common.rs
@@ -1,19 +1,23 @@
 use uuid::Uuid;
 
 /// The configuration that we are currently on.
-pub type ConfigId = i64;
+pub type ConfigId = u64;
 /// The ring identifier for which ring this node is from.
 pub type RingNumber = i32;
 /// Represents some _node/destination_ in the system.
 pub type Endpoint = String;
 
 /// Represents the NodeId internall it is just a Uuid v4
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub struct NodeId(Uuid);
 
 impl NodeId {
     pub fn new() -> Self {
         NodeId(Uuid::new_v4())
+    }
+
+    pub fn as_bytes(&self) -> &[u8] {
+        self.0.as_bytes()
     }
 }
 

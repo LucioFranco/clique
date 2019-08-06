c8c2df1349d05bd02538265a066c9f16ea219232
Lucio Franco <luciofranco14@gmail.com>
2019-08-06 15:41:05 -0400

    Refactor cluster (#30)
    * Refactor cluster
    
    * Fix grpc transport impl
    
    * Fix comment spacing
    
    * fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #![feature(async_await)]
-#![recursion_limit = "256"]
+#![recursion_limit = "512"]
 // #![deny(missing_docs, missing_debug_implementations, rust_2018_idioms)]
 // #![cfg_attr(test, deny(warnings))]
 // #![doc(test(no_crate_inject, attr(deny(rust_2018_idioms))))]
@@ -10,13 +10,20 @@
 #![allow(unused)]
 
 mod alert;
-pub mod cluster;
+mod builder;
+mod cluster;
 mod common;
 mod consensus;
 mod error;
+mod event;
+mod handle;
 mod membership;
 mod monitor;
 pub mod transport;
 
+pub use self::builder::Builder;
 pub use self::cluster::Cluster;
 pub use self::error::{Error, Result};
+pub use self::event::Event;
+pub use self::handle::Handle;
+pub use self::transport::Transport;

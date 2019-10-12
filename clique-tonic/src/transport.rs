861814060f8c1f3832c3cabce8d451a0db4c0309
Bhargav Voleti <voletibv@amazon.com>
2019-10-11 22:49:31 -0700

    getting closer

--- clique-tonic/src/transport.rs
+++ clique-tonic/src/transport.rs
@@ -1,5 +1,5 @@
 use clique::transport;
-use futures::future;
+use futures::{future, Future};
 use tokio::sync::mpsc;
 use tonic::{
     transport::{channel::ResponseFuture, Channel},
@@ -21,11 +21,17 @@ where
     T: Into<String>,
 {
     type Error = crate::Error;
-    type ClientFuture = Result<clique::transport::Response, Error>;
+    type ClientFuture = Box<dyn Future<Output = Result<clique::transport::Response, crate::Error>>>;
 
     fn send(&mut self, req: transport::Request) -> Self::ClientFuture {
         let req = Request::new(req.into_inner().into());
-        self.client.send_request(req)
+        Box::new(async {
+            self.client
+            .send_request(req)
+            .await
+            .map_err(|_| crate::Error::Upstream)?
+            .into_inner()
+        })
     }
 
     type ServerFuture = future::Ready<Result<Self::ServerStream, Self::Error>>;
